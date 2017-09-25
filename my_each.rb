30d5d8eec84799ed82ce5a34dbe945ff0373523f
Aaron Samuel <aasamuelj@gmail.com>
2017-09-25 07:07:50 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -1,21 +1,8 @@
-def my_each(arr)
-  if block_given?
-    i = 0
-    while i < arr.length
-      yield arr[i]
-      i+=1
-    end
-    arr
+def my_each(array)
+  counter = 0
+  while counter < array.length
+    yield array[counter]
+    counter += 1
   end
+  array 
 end
-collection = [1, 2, 3, 4]
-my_each(collection) do |i|
-  puts "\##{i}"
-end
-# This should output:
-# 1
-# 2
-# 3
-# 4
-# and return:
-# #=> [1, 2, 3, 4]
